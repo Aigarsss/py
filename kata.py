2110e86542383096b636153f0c58d6fd2f363a3f
Aigarsss <aigars.uplejs@gmail.com>
2019-02-12 21:40:58 +0200

    test

--- kata.py
+++ kata.py
@@ -703,3 +703,15 @@
 
 ########################
 
+# Write Number in Expanded Form
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+# expanded_form(12) # Should return '10 + 2'
+# expanded_form(42) # Should return '40 + 2'
+# expanded_form(70304) # Should return '70000 + 300 + 4'
+
+def expanded_form(num):
+
+    return num
+
+print(expanded_form(112))
